acb38f4024171eff8c645535c32dde05fbdda9fc
Macaulay5 <macaulaynmaram@gmail.com>
2022-03-15 14:46:25 -0700

    alpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,11 @@ int _islower(int c);
 int _isalpha(int n);
 int print_sign(int n);
 int _abs(int);
-
+int print_sign(int n);
+void jack_bauer(void);
+void times_table(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 #endif
+
