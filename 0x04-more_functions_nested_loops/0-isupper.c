1a55008b1017847eab15143a27206b9a90b024a7
Macaulay5 <macaulaynmaram@gmail.com>
2022-03-17 15:41:15 -0700

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -14,10 +14,10 @@ int _isupper(int c)
 {
 	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	  c = 'a';
-	  printf("%c: %d\n", c, _isupper(c));
-	  return (0);
-	  return (c >= 'A' && c <= 'Z');
+       c = 'A';
+       	printf("%c: %d\n", c, _isupper(c));
+        c = 'a';
+        printf("%c: %d\n", c, _isupper(c));
+        return (0);
+     return (c >= 'A' && c <= 'Z');
 }
